bbe2aab28660869b1f92b8c3c319922d1846715c
Daniel <daniel@kylix.online>
2022-08-16 11:01:14 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+var upperCaseCustomerName = () => {
+    customerName = 'BOB'
+}
+
+var setBestCustomer = () => {
+}
+
+var bestCustomer = setBestCustomer ()
+bestCustomer = "not bob"
+
+var overwriteBestCustomer = () => {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "John"
+
+var changeLeastFavoriteCustomer = () => {
+    leastFavoriteCustomer = "James"
+}
